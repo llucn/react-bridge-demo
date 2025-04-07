f3ebda08798008345223b083d99a1584c257fe0c
Lane Lu <161492667+llucn@users.noreply.github.com>
2025-04-08 00:55:55 +0800

    add bridge function and message type

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { type Bridge, type BridgeStore, linkBridge } from '@webview-bridge/web';
-import { ColorSchemeName, ScaledSize } from '@delta/mobile-workforce-app/src/bridge';
 import './App.css';
-import { SyncPayloadV2, syncPayload } from './syncPayload';
+
+type ColorSchemeName = 'light' | 'dark' | null | undefined;
 
 export interface BridgeState extends Bridge {
   query: (obj: any) => Promise<any>;
-
-	getColorScheme: () => Promise<ColorSchemeName>;
-
-	getDimensions: () => Promise<ScaledSize>;
-
-	setSyncPayload: (payload: SyncPayloadV2) => Promise<void>;
+  getColorScheme: () => Promise<ColorSchemeName>;
+  scanCode: () => Promise<string>;
 }
 
 type AppBridge = BridgeStore<BridgeState>;
 const bridge = linkBridge<AppBridge>();
 
 function App() {
+  const [ colorScheme, setColorScheme ] = useState<ColorSchemeName>();
+  const [ qrCode, setQrCode ] = useState<string>('');
 
-  React.useEffect(() => {
-    bridge.setSyncPayload(syncPayload);
+  const clickGetColorScheme = async () => {
+    const cs = await bridge.getColorScheme();
+    console.log('Webapp: clickGetColorScheme: ', cs);
+    setColorScheme(cs);
+  }
+
+  const scanQrCode = async () => {
+    const id = await bridge.scanCode();
+    console.log('Webapp: scanQrCode: ', id);
+  }
+
+  useEffect(() => {
+    // Subscribe to events from react native.
+    return bridge.addEventListener("scanCodeResult", (message: any) => {
+      console.log('Webapp: scanCodeResult: ', message);
+      setQrCode(message.value);
+    });
   }, []);
-
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          React Bridge Demo
-        </p>
+    <div>
+      <header>
+        <div>
+          <button onClick={clickGetColorScheme}>Get Color Scheme</button>
+          <div>Color Scheme: {colorScheme}</div>
+        </div>
+        <div>
+          <button onClick={scanQrCode}>Scan QR Code</button>
+          <div>Code: {qrCode}</div>
+        </div>
       </header>
     </div>
   );
